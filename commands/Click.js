0b4955118603892faeaeede84dbb889967a7724d
KK21CN <krzysztof.kolodziejczyk21cn@gmail.com>
2019-02-27 20:11:31 +0100

    Delete Click.js

--- commands/Click.js
+++
@@ -1,7 +0,0 @@
-exports.command = function (selector, time, message = `Clicked selector: ${selector}`) {
-	return this
-		.waitForElementVisible(selector, time)
-		.click(selector, () => {
-			console.log(message)
-		})
-}
